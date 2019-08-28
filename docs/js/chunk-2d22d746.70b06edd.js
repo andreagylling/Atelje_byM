6c7fe291a2db4b89dfb4491dbbc50b7ef28e9a2d
Andrea Gylling <andreagylling@gmail.com>
2019-08-28 12:20:56 +0200

    removed docs folder

--- docs/js/chunk-2d22d746.70b06edd.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")]),n("h3",[e._v("More to come:")]),n("p",[e._v("Instagram feed & ateljé photo gallery")])])}],l=n("2877"),o={},r=Object(l["a"])(o,a,s,!1,null,null,null);t["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d22d746.70b06edd.js.map
